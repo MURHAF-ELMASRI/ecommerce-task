d752acf662b990ee9eb49e2ede1300262ae02097
Murhaf ALMASRI <mumasri@geniustags.com>
2024-11-15 16:08:48 +0300

    feat: show overlay when refetching

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -68,7 +68,7 @@ export function HomePage() {
         )}
 
         <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 justify-items-center relative">
-          {infiniteProducts.isFetching && (
+          {infiniteProducts.isRefetching && (
             <div className="absolute w-full h-full bg-slate-800  opacity-50 z-50"></div>
           )}
           {infiniteProducts.isSuccess &&
